4851fcfe30011fc952480493a1561183311168c8
Olgierd Falat <olgierd.falat@gmail.com>
2016-11-17 13:21:57 +0000

    Fixed encoding issue.

--- handler.js
+++ handler.js
@@ -1,16 +1,17 @@
 'use strict';
 
 module.exports.findSheep = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Łowieczka jest tutaj !!!',
-      input: event,
-    }),
-  };
+    const message = 'Łowieczka jest tutaj !!!';
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify({
+            message,
+            input: event,
+        })
+    };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    callback(null, response);
 };
